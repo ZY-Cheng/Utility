e800e5b02c8166b409771823680f4d9f499018aa
zhengshuyu <136682496@qq.com>
2022-07-13 15:09:22 +0800

    feat(types/utils.d.ts): 增加工具类型

--- types/utils.d.ts
+++ types/utils.d.ts
@@ -10,3 +10,21 @@ type Generics<T> = Promise<T>;
 type ExtractGenericsParameter<T> = {
   [K in keyof PickFn<T>]: T[K] extends (arg: Generics<infer P>) => Generics<infer U> ? (arg: P) => Generics<U> : never;
 };
+
+type Shift<T extends any[]> = T extends [infer First, ...infer Rest] ? Rest : [];
+
+type IsUppercase<T extends string> = T extends '' ? false : Uppercase<T> extends T ? true : false;
+
+type FirstChar<T extends string> = T extends `${infer First}${infer Rest}` ? First : never;
+
+type LastChar<T extends string> = T extends `${infer First}${infer Rest}`
+  ? Rest extends ''
+    ? First
+    : LastChar<Rest>
+  : never;
+
+type ToNumber<T extends string, U extends number[] = []> = `${U['length']}` extends T
+  ? U['length']
+  : ToNumber<T, [...U, 1]>;
+
+type IsAny<T> = 0 extends 1 & T ? true : false;
